83e0efcaac61667c634b7e483f68333983a96e59
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-26 10:45:49 +0800

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to decloj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/decloj/core_test.clj
@@ -0,0 +1,7 @@
+(ns decloj.core-test
+  (:require [clojure.test :refer :all]
+            [decloj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
