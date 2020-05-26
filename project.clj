e15f871dedb4ca9078d1495b7187beea0909f3fe
Crispin Wellington <retrogradeorbit@gmail.com>
2020-05-26 12:00:26 +0800

    got it working! thanks @seancorfield

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
+                 [org.bytedeco/qt-platform "5.14.2-1.5.3"]]
   :main ^:skip-aot decloj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/decloj/core.clj
+++ src/decloj/core.clj
@@ -1,7 +1,31 @@
 (ns decloj.core
+  (:require [clojure.java.io :as io])
+  (:import [org.bytedeco.javacpp PointerPointer IntPointer]
+           [org.bytedeco.javacpp Loader]
+           [org.bytedeco.qt.global Qt5Core]
+           [org.bytedeco.qt.Qt5Widgets QApplication QTextEdit]
+           )
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Setup")
+  (let [lib-path (Loader/load Qt5Core)
+        app (QApplication.
+             (IntPointer. (int-array [3]))
+             (PointerPointer.
+              (into-array
+               String
+               ["gettingstarted"
+                "-platformpluginpath"
+                (-> lib-path io/file .getParent)])
+              ))
+        text-edit (QTextEdit.)
+        ]
+    (.show text-edit)
+    (let [result (org.bytedeco.qt.Qt5Widgets.QApplication/exec)]
+      (println "Exiting" result)
+      (System/exit result)))
+
+  )
